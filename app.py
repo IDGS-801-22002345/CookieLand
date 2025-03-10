b348551721aea0f4f5b909b6f1afff11b0baa505
IDGS-801-21000421 <murilloabraham976@gmail.com>
2025-03-10 17:42:18 -0600

    mi primer commit

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask import flash
 from flask_wtf.csrf import CSRFProtect
-from flask import g
-from config import DevelopmentConfig
-import forms
-from models import db
-from models import Alumnos
+
+# Importar rutas correctamente desde el módulo 'routes'
+from routes.cliente_routes import cliente_bp
+from routes.auth_routes import auth_bp
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-csrf=CSRFProtect()
 
-@app.route("/detalles", methods=['GET','POST'])
-def detalles():
-	if request.method=='GET':
-		id=request.args.get('id')
-		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
-		id=request.args.get('id')
-		nombre=alum1.nombre
-		apaterno=alum1.apaterno
-		email=alum1.email
-	return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
+# Protección contra CSRF
+csrf = CSRFProtect()
 
-
-@app.route("/", methods=['GET','POST'])
-@app.route("/index")
-def index():
-	create_form=forms.UserForm2(request.form)
-	alumno= Alumnos.query.all()
-	return render_template("index.html", form=create_form, alumnos=alumno)
+# Rutas importadas
+app.register_blueprint(cliente_bp)
+app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
-	csrf.init_app(app)
-	db.init_app(app)
-	with app.app_context():
-		db.create_all()
-	app.run(debug=True)
+    csrf.init_app(app)
+    app.run(debug=True)
